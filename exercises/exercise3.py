d59d97b59bb11e23473a1295626a2e11554b7353
Divya Dharmapura Manjunath <56184965+divya79105@users.noreply.github.com>
2023-12-13 00:55:31 +0100

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -6,20 +6,15 @@ url = "https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00
 df = pd.read_csv(url, sep=";", encoding='ISO-8859-1', skiprows=6, skipfooter=4, engine='python')
 
 # 2. Reshape data structure
-selected_columns = {
-    'A': 'date',
-    'B': 'CIN',
-    'C': 'name',
-    'M': 'petrol',
-    'W': 'diesel',
-    'AG': 'gas',
-    'AQ': 'electro',
-    'BA': 'hybrid',
-    'BK': 'plugInHybrid',
-    'BU': 'others'
-}
-df = df.rename(columns=selected_columns)
+columns_to_keep = ['Unnamed: 0', 'Unnamed: 1', 'Unnamed: 2', 'Insgesamt', 'Insgesamt.1', 'Insgesamt.2', 'Insgesamt.3', 'Insgesamt.4', 'Insgesamt.5', 'Insgesamt.6']
+new_column_names = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
 
+column_mapping = dict(zip(columns_to_keep, new_column_names))
+
+df.rename(columns=column_mapping, inplace=True)
+
+df = df[new_column_names]
+   
 # 3. Validate data
 # Validate 'name' as a string
 df = df[df['name'].astype(str).apply(lambda x: isinstance(x, str))]
