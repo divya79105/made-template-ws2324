2fb3c2e51555b8e6561a6812a429cfefc913ea49
Divya Dharmapura Manjunath <56184965+divya79105@users.noreply.github.com>
2023-12-13 00:33:34 +0100

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,54 +1,38 @@
 import pandas as pd
 import sqlite3
 
-# 1. Download data
+# Step 1: Download the data
 url = "https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv"
-df = pd.read_csv(url, encoding='latin1', skiprows=6, skipfooter=4, engine='python')
+df = pd.read_csv(url, sep=";", encoding='ISO-8859-1', skiprows=6, skipfooter=4, engine='python')
 
-# 2. Reshape data structure
-selected_columns = {
-    'A': 'date',
-    'B': 'CIN',
-    'C': 'name',
-    'M': 'petrol',
-    'W': 'diesel',
-    'AG': 'gas',
-    'AQ': 'electro',
-    'BA': 'hybrid',
-    'BK': 'plugInHybrid',
-    'BU': 'others'
-}
-df = df.rename(columns=selected_columns)
 
-# 3. Validate data
-name_column = [col for col in df.columns if 'name' in col.lower()]
-CIN_column = [col for col in df.columns if 'cin' in col.lower()]
+# Step 2: Reshape the data structure
+columns_to_keep = ['Unnamed: 0', 'Unnamed: 1', 'Unnamed: 2', 'Insgesamt', 'Insgesamt.1', 'Insgesamt.2', 'Insgesamt.3', 'Insgesamt.4', 'Insgesamt.5', 'Insgesamt.6']
+new_column_names = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
 
-# Additional validation for positive integers > 0 for all other columns
-other_columns = [col for col in df.columns if col not in ['date', 'CIN', 'name']]
-for col in other_columns:
-    df = df[df[col].astype(str).apply(lambda x: x.isdigit() and int(x) > 0)]
+column_mapping = dict(zip(columns_to_keep, new_column_names))
 
-if name_column:
-    df = df[df[name_column[0]].astype(str).apply(lambda x: isinstance(x, str))]  # Validate 'name' as string
+df.rename(columns=column_mapping, inplace=True)
 
-if CIN_column:
-    df = df[df[CIN_column[0]].astype(str).apply(lambda x: len(x) == 5 and x.isdigit() or (x.isdigit() and x.startswith('0')))]  # Validate 'CIN'
+df = df[new_column_names]
 
-# Drop rows with missing or invalid values
-df = df.dropna()
+# Step 3: Validate data
+# Validate CINs
+df['CIN'] = df['CIN'].astype(str).str.zfill(5)
 
-# 4. Use fitting SQLite types for all columns
+# Validate positive integers
+numeric_columns = ['petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
+df = df.loc[(df[numeric_columns].apply(pd.to_numeric, errors='coerce') > 0).all(axis=1)]
+
+print(df)
+
+# Step 4: Use fitting SQLite types
+# Define SQLite types
 sqlite_types = {'date': 'TEXT', 'CIN': 'TEXT', 'name': 'TEXT',
                 'petrol': 'INTEGER', 'diesel': 'INTEGER', 'gas': 'INTEGER',
                 'electro': 'INTEGER', 'hybrid': 'INTEGER', 'plugInHybrid': 'INTEGER', 'others': 'INTEGER'}
 
-# 5. Write data to SQLite database
-db_path = "cars.sqlite"
-table_name = "cars"
-
-conn = sqlite3.connect(db_path)
-df.to_sql(table_name, conn, index=False, if_exists='replace', dtype=sqlite_types)
+# Step 5: Write data to SQLite database
+conn = sqlite3.connect('cars.sqlite')
+df.to_sql('cars', conn, index=False, if_exists='replace', dtype=sqlite_types)
 conn.close()
-
-print(f"Data written to {db_path}, table: {table_name}")
