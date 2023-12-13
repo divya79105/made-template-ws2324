6c5144a96335eb3e1b8bc85f2f6df795d55ec102
Divya Dharmapura Manjunath <56184965+divya79105@users.noreply.github.com>
2023-12-13 02:22:42 +0100

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import sqlite3
 
-# Step 1: Download the data
+#1. Download the data
 url = "https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv"
 df = pd.read_csv(url, sep=";", encoding='ISO-8859-1', skiprows=6, skipfooter=4, engine='python')
 
-# Step 2: Reshape the data structure
+#2. Reshape data structure
 old_columns = ['Unnamed: 0', 'Unnamed: 1', 'Unnamed: 2', 'Insgesamt', 'Insgesamt.1', 'Insgesamt.2', 'Insgesamt.3', 'Insgesamt.4', 'Insgesamt.5', 'Insgesamt.6']
 new_columns = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
 
 
 df = df[old_columns].rename(columns=dict(zip(old_columns, new_columns)))
 
-# Step 3: Validate data
+#3. Validate data
 # Validate CINs
 df['CIN'] = df['CIN'].apply(lambda x: f'{x:0>5}')
 
@@ -25,7 +25,7 @@ df = df[df[numeric_columns].apply(pd.to_numeric, errors='coerce').gt(0).all(axis
 df = df.dropna()
 print(df)
 
-# Step 4: Use fitting SQLite types
+#4. Use fitting SQLite types for all columns
 sqlite_types = {
     'date': 'TEXT',
     'CIN': 'TEXT',
@@ -39,7 +39,7 @@ sqlite_types = {
     'others': 'INTEGER'
 }
 
-# Step 5: Write data to SQLite database
+#5. Write data into a SQLite database
 conn = sqlite3.connect('cars.sqlite')
 df.to_sql('cars', conn, index=False, if_exists='replace', dtype=sqlite_types)
 conn.close()
