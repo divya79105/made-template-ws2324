77b7f2d6ef94dbbf0d673d4430ea1f4d7752d708
Divya Dharmapura Manjunath <56184965+divya79105@users.noreply.github.com>
2023-12-13 01:36:33 +0100

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,59 +1,44 @@
 import pandas as pd
 import sqlite3
 
-# Download data
+# Step 1: Download the data
 url = "https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv"
-
-# Read data while skipping metadata
 df = pd.read_csv(url, sep=";", encoding='ISO-8859-1', skiprows=6, skipfooter=4, engine='python')
 
-# Specify the columns to keep and their new names
-columns_to_keep = {
-    'A': 'date',
-    'B': 'CIN',
-    'C': 'name',
-    'M': 'petrol',
-    'W': 'diesel',
-    'AG': 'gas',
-    'AQ': 'electro',
-    'BA': 'hybrid',
-    'BK': 'plugInHybrid',
-    'BU': 'others'
-}
 
-# Keep only specified columns and rename them
-df = df[columns_to_keep.keys()]
-df = df.rename(columns=columns_to_keep)
+# Step 2: Reshape the data structure
+columns_to_keep = ['Unnamed: 0', 'Unnamed: 1', 'Unnamed: 2', 'Insgesamt', 'Insgesamt.1', 'Insgesamt.2', 'Insgesamt.3', 'Insgesamt.4', 'Insgesamt.5', 'Insgesamt.6']
+new_column_names = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
 
-# Validate 'name' column as a string
+column_mapping = dict(zip(columns_to_keep, new_column_names))
+
+df.rename(columns=column_mapping, inplace=True)
+
+df = df[new_column_names]
+
+# Step 3: Validate data
 df = df[df['name'].astype(str).str.isalpha()]
-
-# Ensure 'CIN' column is a string of length 5
+# Validate CINs
 df['CIN'] = df['CIN'].astype(str).apply(lambda x: x.zfill(5) if x.isdigit() else x)
 
-# Validate other columns as positive integers > 0
+# Validate positive integers
 numeric_columns = ['petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
-
 for col in numeric_columns:
     df = df[df[col].astype(str).apply(lambda x: x.isdigit() and int(x) > 0)]
 
 # Drop rows with missing or invalid values
 df = df.dropna()
 
-# Infer SQLite types for columns
-sqlite_types = df.infer_objects().dtypes.apply(lambda x: 'TEXT' if x == 'object' else x).to_dict()
+print(df)
 
-# Write data to SQLite database
-db_path = "cars.sqlite"
-table_name = "cars"
+# Step 4: Use fitting SQLite types
+# Define SQLite types
+sqlite_types = {'date': 'TEXT', 'CIN': 'TEXT', 'name': 'TEXT',
+                'petrol': 'INTEGER', 'diesel': 'INTEGER', 'gas': 'INTEGER',
+                'electro': 'INTEGER', 'hybrid': 'INTEGER', 'plugInHybrid': 'INTEGER', 'others': 'INTEGER'}
 
-# Connect to SQLite database
-conn = sqlite3.connect(db_path)
-
-# Write DataFrame to SQLite table with inferred types
-df.to_sql(table_name, conn, index=False, if_exists='replace', dtype=sqlite_types)
-
-# Close the database connection
+# Step 5: Write data to SQLite database
+conn = sqlite3.connect('cars.sqlite')
+df.to_sql('cars', conn, index=False, if_exists='replace', dtype=sqlite_types)
 conn.close()
 
-print(f"Data written to {db_path}, table: {table_name}")
