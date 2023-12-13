4f0f375301e48f2404fed05776802072b8e735dc
Divya Dharmapura Manjunath <56184965+divya79105@users.noreply.github.com>
2023-12-13 01:06:24 +0100

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -13,19 +13,16 @@ column_mapping = dict(zip(columns_to_keep, new_column_names))
 df.rename(columns=column_mapping, inplace=True)
 
 # 3. Validate data
-name_column = [col for col in df.columns if 'name' in col.lower()]
-CIN_column = [col for col in df.columns if 'cin' in col.lower()]
-
 # Additional validation for positive integers > 0 for all other columns
 other_columns = [col for col in df.columns if col not in ['date', 'CIN', 'name']]
 for col in other_columns:
     df = df[df[col].astype(str).apply(lambda x: x.isdigit() and int(x) > 0)]
 
-if name_column:
-    df = df[df[name_column[0]].astype(str).apply(lambda x: isinstance(x, str))]  # Validate 'name' as string
+# Validate 'name' as a string
+df = df[df['name'].astype(str).apply(lambda x: isinstance(x, str))]
 
-if CIN_column:
-    df = df[df[CIN_column[0]].astype(str).apply(lambda x: len(x) == 5 and x.isdigit() or (x.isdigit() and x.startswith('0')))]  # Validate 'CIN'
+# Validate 'CIN'
+df = df[df['CIN'].astype(str).apply(lambda x: len(x) == 5 and x.isdigit() or (x.isdigit() and x.startswith('0')))]
 
 # Drop rows with missing or invalid values
 df = df.dropna()
@@ -34,7 +31,13 @@ df = df.dropna()
 sqlite_types = {'date': 'TEXT', 'CIN': 'TEXT', 'name': 'TEXT',
                 'petrol': 'INTEGER', 'diesel': 'INTEGER', 'gas': 'INTEGER',
                 'electro': 'INTEGER', 'hybrid': 'INTEGER', 'plugInHybrid': 'INTEGER', 'others': 'INTEGER'}
+
 # 5. Write data to SQLite database
-conn = sqlite3.connect('cars.sqlite')
-df.to_sql('cars', conn, index=False, if_exists='replace', dtype=sqlite_types)
+db_path = "cars.sqlite"
+table_name = "cars"
+
+conn = sqlite3.connect(db_path)
+df.to_sql(table_name, conn, index=False, if_exists='replace', dtype=sqlite_types)
 conn.close()
+
+print(f"Data written to {db_path}, table: {table_name}")
