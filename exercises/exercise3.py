7f663c1b679a0fad6afaf3834a533ef40b2d7324
Divya Dharmapura Manjunath <56184965+divya79105@users.noreply.github.com>
2023-12-13 01:00:45 +0100

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -3,46 +3,39 @@ import sqlite3
 
 # 1. Download data
 url = "https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv"
-df = pd.read_csv(url, sep=";", encoding='ISO-8859-1', skiprows=6, skipfooter=4, engine='python')
+df = pd.read_csv(url, encoding='latin1', skiprows=6, skipfooter=4, engine='python')
 
 # 2. Reshape data structure
 columns_to_keep = ['Unnamed: 0', 'Unnamed: 1', 'Unnamed: 2', 'Insgesamt', 'Insgesamt.1', 'Insgesamt.2', 'Insgesamt.3', 'Insgesamt.4', 'Insgesamt.5', 'Insgesamt.6']
 new_column_names = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
 
 column_mapping = dict(zip(columns_to_keep, new_column_names))
-
 df.rename(columns=column_mapping, inplace=True)
 
-df = df[new_column_names]
-   
 # 3. Validate data
-# Validate 'name' as a string
-df = df[df['name'].astype(str).apply(lambda x: isinstance(x, str))]
+name_column = [col for col in df.columns if 'name' in col.lower()]
+CIN_column = [col for col in df.columns if 'cin' in col.lower()]
 
-# Validate 'CIN' as a string with length 5 or starting with '0'
-df = df[df['CIN'].astype(str).apply(lambda x: x.isdigit() and (len(x) == 5 or x.startswith('0')))]
-
-# Validate positive integers > 0 for all other columns
+# Additional validation for positive integers > 0 for all other columns
 other_columns = [col for col in df.columns if col not in ['date', 'CIN', 'name']]
 for col in other_columns:
     df = df[df[col].astype(str).apply(lambda x: x.isdigit() and int(x) > 0)]
 
+if name_column:
+    df = df[df[name_column[0]].astype(str).apply(lambda x: isinstance(x, str))]  # Validate 'name' as string
+
+if CIN_column:
+    df = df[df[CIN_column[0]].astype(str).apply(lambda x: len(x) == 5 and x.isdigit() or (x.isdigit() and x.startswith('0')))]  # Validate 'CIN'
+
 # Drop rows with missing or invalid values
 df = df.dropna()
 
-# Print for debugging
-print("Data after reshaping and validation:")
-print(df.head())
-
 # 4. Use fitting SQLite types for all columns
-sqlite_types = {'date': 'TEXT', 'CIN': 'TEXT', 'name': 'TEXT',
-                'petrol': 'INTEGER', 'diesel': 'INTEGER', 'gas': 'INTEGER',
-                'electro': 'INTEGER', 'hybrid': 'INTEGER', 'plugInHybrid': 'INTEGER', 'others': 'INTEGER'}
+sqlite_types = {col: 'TEXT' if df[col].dtype == 'O' else 'REAL' for col in df.columns}
 
 # 5. Write data to SQLite database
-conn = sqlite3.connect('cars.sqlite')
-df.to_sql('cars', conn, index=False, if_exists='replace', dtype=sqlite_types)
-conn.close()
+db_path = "cars.sqlite"
+table_name = "cars"
 
 conn = sqlite3.connect(db_path)
 df.to_sql(table_name, conn, index=False, if_exists='replace', dtype=sqlite_types)
