dfdf340d50b5f6f2c70baa1c5688ed0dfed4e376
Divya Dharmapura Manjunath <56184965+divya79105@users.noreply.github.com>
2023-12-13 01:20:36 +0100

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,33 +1,65 @@
 import pandas as pd
 import sqlite3
 
-# 1. Download data
+# Download data
 url = "https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv"
-df = pd.read_csv(url, sep=";", encoding='ISO-8859-1', skiprows=6, skipfooter=4, engine='python')
+df = pd.read_csv(url, encoding='latin1', skiprows=6, skipfooter=4, engine='python')
 
-# 2. Reshape data structure
-# Include all columns from the source data
-new_column_names = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
-df.columns = new_column_names
+# Keep only selected columns and rename them
+selected_columns = {
+    'A': 'date',
+    'B': 'CIN',
+    'C': 'name',
+    'M': 'petrol',
+    'W': 'diesel',
+    'AG': 'gas',
+    'AQ': 'electro',
+    'BA': 'hybrid',
+    'BK': 'plugInHybrid',
+    'BU': 'others'
+}
 
-# 3. Validate data
-df['CIN'] = df['CIN'].astype(str).str.zfill(5)
+df = df.rename(columns=selected_columns)
 
-# Validate positive integers
-numeric_columns = ['petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
-df = df.loc[(df[numeric_columns].apply(pd.to_numeric, errors='coerce') > 0).all(axis=1)]
+# Identify the actual column names
+name_column = [col for col in df.columns if 'name' in col.lower()]
+CIN_column = [col for col in df.columns if 'cin' in col.lower()]
 
-# 4. Use fitting SQLite types for all columns
-sqlite_types = {'date': 'TEXT', 'CIN': 'TEXT', 'name': 'TEXT',
-                'petrol': 'INTEGER', 'diesel': 'INTEGER', 'gas': 'INTEGER',
-                'electro': 'INTEGER', 'hybrid': 'INTEGER', 'plugInHybrid': 'INTEGER', 'others': 'INTEGER'}
+# Data validation
+if name_column:
+    df = df[df[name_column[0]].astype(str).apply(lambda x: isinstance(x, str))]  # Validate 'name' as string
 
-# 5. Write data to SQLite database
+if CIN_column:
+    df = df[df[CIN_column[0]].astype(str).apply(lambda x: len(x) == 5 and x.isdigit() or (x.isdigit() and x.startswith('0')))]  # Validate 'CIN'
+
+# Drop rows with missing or invalid values
+df = df.dropna()
+
+# Specify SQLite types for columns
+sqlite_types = {
+    'date': 'TEXT',
+    'CIN': 'TEXT',
+    'name': 'TEXT',
+    'petrol': 'FLOAT',
+    'diesel': 'FLOAT',
+    'gas': 'FLOAT',
+    'electro': 'FLOAT',
+    'hybrid': 'FLOAT',
+    'plugInHybrid': 'FLOAT',
+    'others': 'FLOAT'
+}
+
+# Write data to SQLite database
 db_path = "cars.sqlite"
 table_name = "cars"
 
+# Connect to SQLite database
 conn = sqlite3.connect(db_path)
+
+# Write DataFrame to SQLite table with specified types
 df.to_sql(table_name, conn, index=False, if_exists='replace', dtype=sqlite_types)
+
+# Close the database connection
 conn.close()
 
 print(f"Data written to {db_path}, table: {table_name}")
