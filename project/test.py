165888f5cdc7fa9d3e4a2001150a7adfaff81507
Divya Dharmapura Manjunath <56184965+divya79105@users.noreply.github.com>
2023-12-18 19:16:07 +0100

    Update test.py

--- project/test.py
+++ project/test.py
@@ -1,9 +1,10 @@
 import unittest
 import sqlite3
+import pandas as pd
+import opendatasets as od
 
 class TestDataProcessing(unittest.TestCase):
-
-    def execute_query(self, conn, query):
+    def execute_query(conn, query):
         try:
             cursor = conn.execute(query)
             result = cursor.fetchall()
@@ -12,47 +13,44 @@ class TestDataProcessing(unittest.TestCase):
         except Exception as e:
             raise Exception(f"Error executing SQL query: {e}")
 
-    def print_tables(self, conn, db_name):
+    def print_tables(conn, db_name):
         query = f"SELECT name FROM sqlite_master WHERE type='table';"
-        tables = self.execute_query(conn, query)
+        tables = execute_query(conn, query)
         print(f"Tables in {db_name}: {tables}")
-
-    def get_column_names(self, conn, table_name):
-        query = f"PRAGMA table_info({table_name});"
-        columns = self.execute_query(conn, query)
-        column_names = [column[1] for column in columns]
-        return column_names
 
     def setUp(self):
         try:
             # Set up SQLite databases
-            self.conn1 = sqlite3.connect('../data/hotel_bookings.sqlite')
-            self.table1 = 'hotel_bookings'
-            self.columns1 = self.get_column_names(self.conn1, self.table1)
-            self.print_tables(self.conn1, 'hotel_bookings.sqlite')
-    
-            self.conn2 = sqlite3.connect('../data/weather_data.sqlite')
+            od.download("https://www.kaggle.com/datasets/jessemostipak/hotel-booking-demand")
+            od.download("https://www.kaggle.com/datasets/muthuj7/weather-dataset")
+
+            hotel_booking_data = pd.read_csv("hotel-booking-demand/hotel_bookings.csv")
+            weather_data = pd.read_csv("weather-dataset/weatherHistory.csv")
+
+            self.conn1 = sqlite3.connect("../data/hotel_booking.sqlite")
+            hotel_booking_data.to_sql("hotel_booking", self.conn1, index=False, if_exists="replace")
+
+            self.conn2 = sqlite3.connect("../data/weather_data.sqlite")
+            weather_data.to_sql("weather_data", self.conn2, index=False, if_exists="replace")
+
+            # Set the table and columns for hotel_bookings
+            self.table1 = 'hotel_booking'
+            self.columns1 = hotel_booking_data.columns.tolist()
+
+            # Set the table and columns for weather_data
             self.table2 = 'weather_data'
-            self.columns2 = self.get_column_names(self.conn2, self.table2)
-            self.print_tables(self.conn2, 'weather_data.sqlite')
-    
+            self.columns2 = weather_data.columns.tolist()
+
         except Exception as e:
             self.fail(f"Failed to set up test environment: {e}")
 
     def test_hotelbooking_table_exists(self):
         try:
-            # Test if the hotel_bookings table exists in the database
+            # Print available tables to help with debugging
+            print(f"Tables in {self.db_path1}: {execute_query(self.conn1, 'SELECT name FROM sqlite_master WHERE type=\"table\";')}")
+
+            # Test if the hotel_booking table exists in the database
             cursor = self.conn1.cursor()
-            cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-            table_names = [table[0] for table in cursor.fetchall()]
-            print(f"Tables in hotel_bookings.sqlite: {table_names}")  # Debugging statement
-    
-            cursor.execute("SELECT sql FROM sqlite_master WHERE type='table';")
-            table_schema = cursor.fetchall()
-            print("Schema of tables:")
-            for schema in table_schema:
-                print(schema[0])  # Debugging statement
-    
             cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{self.table1}';")
             tables = cursor.fetchall()
             table_names = [table[0] for table in tables]
@@ -61,43 +59,7 @@ class TestDataProcessing(unittest.TestCase):
         except Exception as e:
             self.fail(f"Test failed: {e}")
 
-
-
-    def test_weather_data_table_exists(self):
-        try:
-            cursor = self.conn2.cursor()
-            cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{self.table2}';")
-            tables = cursor.fetchall()
-            table_names = [table[0] for table in tables]
-            self.assertIn(self.table2, table_names, f"Test failed: {self.table2} table does not exist in the database.")
-            print(f"Test passed: {self.table2} table exists in the database.")
-        except Exception as e:
-            self.fail(f"Test failed: {e}")
-
-    def test_weather_data_table_columns(self):
-        try:
-            # Test if the weather data table has the expected columns
-            cursor = self.conn2.cursor()
-            cursor.execute(f"PRAGMA table_info({self.table2});")
-            table_columns = [column[1] for column in cursor.fetchall()]
-            for column in self.columns2:
-                self.assertIn(column, table_columns, f"Column '{column}' not found in {self.table2}.")
-            print(f"Test passed: {self.table2} has the expected columns.")
-        except Exception as e:
-            self.fail(f"Test failed: {e}")
-
-    def test_hotelbooking_table_all_non_null_values(self):
-        try:
-            # Test if all columns in the hotel_bookings table have non-null values
-            for column in self.columns1:
-                query = f"SELECT COUNT(*) FROM {self.table1} WHERE {column} IS NOT NULL;"
-                cursor = self.conn1.cursor()
-                cursor.execute(query)
-                count = cursor.fetchone()[0]
-                self.assertTrue(count > 0, f"Column '{column}' has null values in {self.table1}.")
-            print(f"Test passed: No null values found in all columns of {self.table1}.")
-        except Exception as e:
-            self.fail(f"Test failed: {e}")
+    # Add other test methods...
 
 if __name__ == "__main__":
     unittest.main()
