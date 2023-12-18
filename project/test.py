73e4f1582e2b1f03cdc97935c1c5e0e4d5493281
Divya Dharmapura Manjunath <56184965+divya79105@users.noreply.github.com>
2023-12-18 19:22:35 +0100

    Update test.py

--- project/test.py
+++ project/test.py
@@ -47,7 +47,7 @@ class TestDataProcessing(unittest.TestCase):
     def test_hotelbooking_table_exists(self):
         try:
             # Print available tables to help with debugging
-            cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{self.table1}';")
+            print(f"Tables in {self.db_path1}: {execute_query(self.conn1, 'SELECT name FROM sqlite_master WHERE type=\"table\";')}")
 
 
             # Test if the hotel_booking table exists in the database
