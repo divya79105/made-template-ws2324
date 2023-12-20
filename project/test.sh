d879dbd71a89a16631f68157f5d7d9e89836e93a
Divya Dharmapura Manjunath <56184965+divya79105@users.noreply.github.com>
2023-12-20 14:55:18 +0100

    Update test.sh

--- project/test.sh
+++ project/test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Install Python packages
-pip install pandas
-pip install pysqlite3
-pip install kaggle
-pip install opendatasets
+pip install pandas pysqlite3 kaggle opendatasets
+
+# Navigate to the correct directory
+cd /home/runner/work/made-template-ws2324/made-template-ws2324/
+
+# Run the Python scripts
 python3 project_pipeline.py
 python3 test.py
